9d8f3c2b114fe71d035fcdcd938379b40d7dd0d8
daniel <daniel@epsilon>
2007-08-10 03:21:49 +0000

    add Silence to quiet other services servers
    svn-rev: trunk@281

---
+++ Silence.pm
@@ -0,0 +1,35 @@
+# Copyright (C) 2007 Daniel De Graaf
+# Released under the Affero General Public License
+# http://www.affero.org/oagpl.html
+package Silence;
+use Persist;
+use strict;
+use warnings;
+
+our($VERSION) = '$Rev$' =~ /(\d+)/;
+
+__PERSIST__
+__CODE__
+
+sub service {
+	my $n = shift;
+	my $srv = $n->info('home_server');
+	return 0 unless $srv;
+	return 0 if $srv =~ /^services\.qliner/;
+	return 1 if $srv =~ /^stats\./;
+	return 1 if $srv =~ /^service.*\..*\./;
+}
+
+&Janus::hook_add(
+	MSG => check => sub {
+		my $act = shift;
+		my $src = $act->{src};
+		my $dst = $act->{dst};
+		return undef unless $srv->isa('Nick') && $dst->isa('Nick');
+		return 1 if service($src);
+		return 1 if service($dst);
+		undef;
+	},
+);
+
+1;
