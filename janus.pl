7c42195d0e3469f98ba7690b604dbd64cd2b55fd
Daniel De Graaf <daniel@epsilon>
2007-09-08 12:22:29 -0500

    Better describe taint support; remove janus-{e,}poll
    The alternate janus socket loops have not been updated because they were not
    used actively. If there is reason for them to be used, they can be recovered
    from history; they are just causing confustion by existing in head.

--- janus.pl
+++ janus.pl
@@ -4,7 +4,12 @@
 # http://www.affero.org/oagpl.html
 use strict;
 BEGIN {
+	# Support for taint mode: we don't acually need most of these protections
+	# as the person running janus.pl is assumed to have shell access anyway.
+	# The real benefit of taint mode is protecting IRC-sourced data
 	$ENV{PATH} = '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin';
+	$ENV{SHELL} = '/bin/sh';
+	delete @ENV{'IFS', 'CDPATH', 'ENV', 'BASH_ENV'};
 	push @INC, '.';
 }
 use Janus;
