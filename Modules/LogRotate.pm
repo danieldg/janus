0006e8353537e56420757db2bf4f433f143cba63
Nima <nima@ircdotcom.com>
2007-11-10 14:26:55 -0600

    Add date formats to LogRotate from config
    Example:
    set {
            datefmt %Y-%m-%d-%H:%M
    }

--- Modules/LogRotate.pm
+++ Modules/LogRotate.pm
@@ -7,7 +7,15 @@ use warnings;
 
 our($VERSION) = '$Rev$' =~ /(\d+)/;
 
-my $log = 'log/'.time;
+my $log = 'log/';
+my $fmt = $Conffile::netconf{janus}{datefmt};
+if ($fmt) {
+	use POSIX qw(strftime);
+	$log .= strftime $fmt, gmtime;
+} else {
+	$log .= time;
+}
+
 umask 022;
 open STDOUT, '>', $log or die $!;
 open STDERR, '>&', \*STDOUT or die $!;
