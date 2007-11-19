d1e444464425139b9941bd31bdcb7b964e53b430
Daniel De Graaf <daniel@epsilon>
2007-11-19 15:29:05 -0600

    Add multi-network claim to Quarrantine

--- Modules/Quarrantine.pm
+++ Modules/Quarrantine.pm
@@ -8,27 +8,41 @@ use Persist;
 
 our($VERSION) = '$Rev$' =~ /(\d+)/;
 
-my @home :PersistAs(Channel, homenet);
+my @homes :PersistAs(Channel, homenets);
 
 &Janus::command_add({
 	cmd => 'claim',
 	help => 'Claim network ownership of a channel',
+	details => [
+		"Syntax: \002CLAIM\002 #channel net[,net,net...]",
+		"Claims network ownership for a channel. Opers and services outside this",
+		"network cannot make mode changes or kicks to this channel.",
+		'-',
+		"Currently this is reset on a link of a channel, and not persisted between",
+		"network restarts. Restricted to opers because of possible conflict with services.",
+	],
 	code => sub {
 		my $nick = shift;
 		my($cname, $nname) = $_[0] =~ /(#\S*)(?: (\S+))?/;
-		my $net = defined $nname ? $Janus::nets{$nname} : $nick->homenet();
+		my $nname ||= $nick->homenet()->name();
 		my $chan = $nick->homenet()->chan($cname) or return;
 		return &Janus::jmsg($nick, "You must be an IRC operator to use this command") unless $nick->has_mode('oper');
-		my $oldhome = $home[$$chan];
-		if ($oldhome && $Janus::nets{$oldhome} && $nick->homenet()->name() ne $oldhome && 
-				$chan->is_on($Janus::nets{$oldhome})) {
-			&Janus::jmsg($nick, "Someone from the $oldhome network must run this command");
-			return;
+		my $oldhomes = $homes[$$chan];
+		if ($oldhomes) {
+			my($claimed, $mine) = (0,0);
+			for (split /,/, $oldhomes) {
+				$claimed++ if $Janus::nets{$_};
+				$mine++ if $nick->homenet()->name() eq $_;
+			}
+			if ($claimed && !$mine) {
+				&Janus::jmsg($nick, "Someone from one of the following networks must run this command: $oldhomes");
+				return;
+			}
 		}
-		my $owner = $net ? $net->name() : undef;
-		$home[$$chan] = $owner;
-		$owner ||= 'unset';
-		&Janus::jmsg($nick, "The owner of $cname is now $owner");
+		$nname = undef if $nname eq 'none' || $nname eq 'unset' || $nname eq 'janus';
+		$homes[$$chan] = $nname;
+		$nname ||= 'unset';
+		&Janus::jmsg($nick, "The owner of $cname is now $nname");
 	},
 });
 
@@ -36,9 +50,9 @@ sub acl_ok {
 	my $act = shift;
 	my $src = $act->{src} or return 1;
 	my $chan = $act->{dst};
-	my $home = $home[$$chan] or return 1;
+	my $home = $homes[$$chan] or return 1;
 	my $snet = $src->isa('Network') ? $src : $src->homenet();
-	return 1 if $snet->name() eq $home;
+	$snet->name() eq $_ and return 1 for split /,/, $home;
 	if ($src->isa('Nick')) {
 		for (qw/n_owner n_admin n_op n_halfop/) {
 			return 1 if $chan->has_nmode($_, $src);
@@ -62,8 +76,8 @@ sub acl_ok {
 		return undef if acl_ok($act);
 		my $src = $act->{src};
 		my $snet = $src->isa('Network') ? $src : $src->homenet();
-		return if $act->{nojlink}; # this is a slight hack, prevents reverting kills
-		return if $snet->jlink();
+		return undef if $act->{nojlink}; # this is a slight hack, prevents reverting kills
+		return undef if $snet->jlink();
 		my $kicked = $act->{kickee};
 		my $chan = $act->{dst};
 		$snet->send(+{
