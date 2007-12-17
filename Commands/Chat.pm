b1cd28c8dd2ad978fb4030cf5e6535398f28925e
Daniel De Graaf <daniel@epsilon>
2007-12-17 14:44:38 -0600

    Remove CHATOPS in favor of CHATTO *

--- Commands/Chat.pm
+++ Commands/Chat.pm
@@ -7,38 +7,23 @@ use warnings;
 our($VERSION) = '$Rev$' =~ /(\d+)/;
 
 &Janus::command_add({
-	cmd => 'chatops',
-	help => 'Send a messge to opers on all other networks',
-	details => [
-		"Syntax: \002CHATOPS\002 message",
-		'The command /chatops, if available, can also be relayed',
-	],
-	code => sub {
-		my($nick,$msg) = @_;
-		return &Janus::jmsg($nick, "You must be an IRC operator to use this command") unless $nick->has_mode('oper');
-		&Janus::append(+{
-			type => 'CHATOPS',
-			src => $nick,
-			sendto => [ values %Janus::nets ],
-			msg => $msg,
-		});
-	},
-}, {
 	cmd => 'chatto',
 	help => 'Send a message to all opers on a specific network',
 	details => [
-		"Syntax: \002CHATTO\002 network message",
+		"Syntax: \002CHATTO\002 network|* message",
+		'Note: The command /chatops, if available, is relayed to all networks',
 	],
 	code => sub {
 		my($nick,$msg) = @_;
 		return &Janus::jmsg($nick, "You must be an IRC operator to use this command") unless $nick->has_mode('oper');
 		$msg =~ s/^(\S+) // or return;
-		my $net = $Janus::nets{$1} or return;
+		my $net = $Janus::nets{$1};
+		return unless $net || $1 eq '*';
 		&Janus::append(+{
 			type => 'CHATOPS',
 			src => $nick,
-			dst => $net,
 			msg => $msg,
+			sendto => ($1 eq '*' ? [ values %Janus::nets ] : [ $net ]),
 		});
 	},
 });
