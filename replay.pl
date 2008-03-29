9cd9ec8a8d912b2cde39bae1ea784e5f4e34834d
Daniel De Graaf <daniel@epsilon>
2008-03-29 17:35:17 -0500

    Correct a few problems noticed while replaying a real log

--- replay.pl
+++ replay.pl
@@ -36,6 +36,17 @@ use constant {
 };
 my $state = NONE;
 
+sub find_ij {
+	my $cid = shift;
+	for (@Connection::queues) {
+		my $net = $_->[&Connection::NET];
+		next unless $net->isa('Server::InterJanus');
+		next unless $net->id() eq $cid;
+		return $net;
+	}
+	undef;
+}
+
 while (<$log>) {
 	if (/\e\[33m   ACTION <INIT( .*>)\e\[m/) {
 		$state = NONE;
@@ -50,19 +61,23 @@ while (<$log>) {
 			conn => sub { 1 },
 			addr => sub { 'nowhere', 5 },
 		);
+		for (values %Conffile::netconf) {
+			$_->{autoconnect} = 0;
+		}
+	} elsif (/^\e\[33m   ACTION <RUN>/) {
 		&Janus::insert_full({ type => 'RUN' });
 	} elsif (/^\e\[36m Autoconnecting (\S+)\e\[m$/) {
 		$state = NONE;
 		my $id = $1;
 		next if $Janus::nets{$id} || $Janus::ijnets{$id};
 		my $nconf = $Conffile::netconf{$id} or die $id;
-		my $type = 'Server::'.$nconf->{type};
-		&Janus::load($type) or die;
-		my $net = &Persist::new($type, id => $id);
-		$net->intro($nconf);
-		&Janus::append({ type => NETLINK => net => $net });
-	} elsif (/^\e\[34m   OUT/) {
-		next if $state == DUMP;
+		$nconf->{autoconnect} = 1;
+		unshift @Janus::qstack, [];
+		&Conffile::connect_net(undef, $id);
+		$nconf->{autoconnect} = 0;
+		&Janus::_runq(shift @Janus::qstack);
+	} elsif (/^\e\[34m   OUT\@(\S+)/) {
+		next if $state == DUMP || !$Janus::nets{$1};
 		$state = DUMP;
 		$_->[&Connection::NET]->dump_sendq() for @Connection::queues;
 	} elsif (/^\e\[32m    IN\@(\S*) (<InterJanus( .*>))\e\[0?m$/) {
@@ -71,14 +86,8 @@ while (<$log>) {
 		$_ = $3;
 		$EventDump::INST->kv_pairs($tmp);
 		die if $cid && $tmp->{id} ne $cid;
-		$cid = $tmp->{id};
+		$ij = find_ij $tmp->{id};
 
-		for (@Connection::queues) {
-			my $net = $_->[&Connection::NET];
-			next unless $net->isa('Server::InterJanus');
-			next unless $net->id() eq $cid;
-			$ij = $net;
-		}
 		$line =~ s/ts="\d+"/ts="$Janus::time"/;
 		if ($ij) {
 			&Janus::insert_full($ij->parse($line));
@@ -91,12 +100,23 @@ while (<$log>) {
 	} elsif (/^\e\[32m    IN\@(\S+) (.*)\e\[0?m$/) {
 		$state = IN;
 		my($nid, $line) = ($1,$2);
-		my $net = $Janus::nets{$nid} || $Janus::ijnets{$nid} || die;
+		my $net = $Janus::nets{$nid} || $Janus::ijnets{$nid};
+		if (!$net) {
+			print "Unknown network in: $_\n";
+			die;
+		}
 		&Janus::in_socket($net, $line);
-	} elsif (/^\e\[33m   ACTION <NETSPLIT( .*>)\e\[m$/) {
+	} elsif (/^\e\[33m   ACTION <(J?NETSPLIT)( .*>)\e\[m$/) {
 		$state = IN;
-		my $act = { type => 'NETSPLIT' };
+		my $act = { type => $1 };
+		my $txt = $_ = $2;
 		$EventDump::INST->kv_pairs($act);
+		unless (defined $act->{net}) {
+			# probably split an IJ net before it was introduced
+			if ($txt =~ / net=j:(\S+) /) {
+				$act->{net} = find_ij $1;
+			}
+		}
 		&Janus::insert_full($act);
 	} else {
 		$state = NONE;
