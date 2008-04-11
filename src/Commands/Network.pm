c31e17e6566b5df675c442f1da247697973198c7
Daniel De Graaf <daniel@epsilon>
2008-04-11 12:35:00 -0500

    Save before running die or restart

--- src/Commands/Network.pm
+++ src/Commands/Network.pm
@@ -31,6 +31,7 @@ use warnings;
 			&Janus::jmsg($nick, "You must specify the 'diepass' password to use this command");
 			return;
 		}
+		&Commands::Save::save() if $Janus::modules{'Commands::Save'};
 		for my $net (values %Janus::nets) {
 			next if $net->jlink();
 			&Janus::append(+{
@@ -58,6 +59,7 @@ use warnings;
 			&Janus::jmsg($nick, "You must specify the 'diepass' password to use this command");
 			return;
 		}
+		&Commands::Save::save() if $Janus::modules{'Commands::Save'};
 		for my $net (values %Janus::nets) {
 			next if $net->jlink();
 			&Janus::append(+{
--- src/Commands/Save.pm
+++ src/Commands/Save.pm
@@ -5,25 +5,30 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+sub save {
+	my $out = $Conffile::netconf{set}{save};
+	my(@vars,@refs);
+	keys %Janus::states;
+	while (my($class,$vars) = each %Janus::states) {
+		keys %$vars;
+		while (my($var,$val) = each %$vars) {
+			push @vars, $val;
+			push @refs, '*'.$class.'::'.$var;
+		}
+	}
+	open my $f, '>', $out or return 0;
+	print $f Data::Dumper->Dump(\@vars, \@refs);
+	close $f;
+	return 1;
+}
+
 &Janus::command_add({
 	cmd => 'save',
 	help => 'Save janus state to filesystem',
 	acl => 1,
 	code => sub {
 		my($nick,$args) = @_;
-		my $out = $Conffile::netconf{set}{save};
-		my(@vars,@refs);
-		keys %Janus::states;
-		while (my($class,$vars) = each %Janus::states) {
-			keys %$vars;
-			while (my($var,$val) = each %$vars) {
-				push @vars, $val;
-				push @refs, '*'.$class.'::'.$var;
-			}
-		}
-		if (open my $f, '>', $out) {
-			print $f Data::Dumper->Dump(\@vars, \@refs);
-			close $f;
+		if (save()) {
 			&Janus::jmsg($nick, 'Saved');
 		} else {
 			&Janus::jmsg($nick, "Could not save: $!");
