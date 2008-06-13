60217fd5ba9c0cb7f68b2d1b634ecb8d7e124b6c
Daniel De Graaf <daniel@epsilon>
2008-06-13 13:10:24 -0500

    Allow "*" to be specified in LIST

--- src/Commands/List.pm
+++ src/Commands/List.pm
@@ -13,27 +13,40 @@ use warnings;
 	code => sub {
 		my($nick,$args) = @_;
 
-		unless ($args && $args =~ /^\S+$/ && $Janus::nets{$args}) {
-			&Janus::jmsg($nick, "Syntax: \002LIST\002 network");
-			return;
-		}
-
-		my $avail = $Link::avail{$args} || {};
-		my @out;
-		for my $chan (keys %$avail) {
-			# TODO filter out rejected channels
-			if ($nick->has_mode('oper')) {
-				push @out, $chan.' '.$avail->{$chan}{mask}.' '.gmtime($avail->{$chan}{time});
-			} else {
-				push @out, $chan;
+		if ($args && $args =~ /^\S+$/ && $Janus::nets{$args}) {
+			my $avail = $Link::avail{$args} || {};
+			my @out;
+			for my $chan (sort keys %$avail) {
+				# TODO filter out rejected channels
+				if ($nick->has_mode('oper')) {
+					push @out, $chan.' '.$avail->{$chan}{mask}.' '.gmtime($avail->{$chan}{time});
+				} else {
+					push @out, $chan;
+				}
 			}
-		}
-		if (@out) {
-			&Janus::jmsg($nick, @out);
+			if (@out) {
+				&Janus::jmsg($nick, @out);
+			} else {
+				&Janus::jmsg($nick, 'No shared channels for that network');
+			}
+		} elsif ($args && $args eq '*') {
+			for my $net (sort keys %Janus::nets) {
+				my $avail = $Link::avail{$net} or next;
+				my @out;
+				for my $chan (sort keys %$avail) {
+					# TODO filter out rejected channels
+					if ($nick->has_mode('oper')) {
+						push @out, $net.$chan.' '.$avail->{$chan}{mask}.' '.gmtime($avail->{$chan}{time});
+					} else {
+						push @out, $net.$chan;
+					}
+				}
+				&Janus::jmsg($nick, @out);
+			}
 		} else {
-			&Janus::jmsg($nick, 'No shared channels for that network');
+			&Janus::jmsg($nick, "Syntax: LIST network");
 		}
-	},	
+	},
 });
 
 1;
