0ba734f898a1c880fe9c763bcacbdb84e001f707
Daniel De Graaf <daniel@epsilon>
2008-06-21 23:05:47 -0500

    Rework Modules::Claim to restrict to home network

--- src/Modules/Claim.pm
+++ src/Modules/Claim.pm
@@ -1,60 +1,54 @@
 # Copyright (C) 2007-2008 Daniel De Graaf
 # Released under the GNU Affero General Public License v3
-package Modules::Quarrantine;
+package Modules::Claim;
 use strict;
 use warnings;
 use Persist;
 
-our @cache;
 our %claim;
-
-&Persist::register_vars('Channel::cache' => \@cache);
 &Janus::save_vars(claim => \%claim);
-
-sub resolve {
-	my $chan = shift;
-	return $cache[$$chan] if defined $cache[$$chan];
-
-	my @list;
-	for my $net ($chan->nets()) {
-		my $nn = $net->name();
-		my $name = $chan->str($net);
-		my $reg = $claim{$nn}{$name};
-		push @list, $nn if $reg;
-	}
-
-	$cache[$$chan] = join ',', @list; # and return it
-}
-
 
 &Janus::command_add({
 	cmd => 'claim',
 	help => 'Claim network ownership of a channel',
 	details => [
 		"\002CLAIM\002 #channel            Lists claims on the channel",
-		"\002CLAIM\002 #channel network    Registers the given network as allowed to operoverride",
-		"\002CLAIM\002 #channel -network   Removes the given network from the allowed list",
+		"\002CLAIM\002 #channel net,net    Sets the claiming networks of the channel",
+		"\002CLAIM\002 #channel -          Removes all claim from the channel",
 		"This command claims network ownership for a channel. Unless the list is empty, only",
 		"networks on the list can have services or opers act on the channel.",
 	],
 	acl => 1,
 	code => sub {
 		my $nick = shift;
-		my($cname, $nname) = $_[0] =~ /(#\S*)(?: (\S+))?/;
-		my $chan = $nick->homenet()->chan($cname) or return;
-		if ($nname) {
-			$nname =~ s/^\+//; # leading + is optional
-			my $off = ($nname =~ s/^-//);
-			my $dnet = $Janus::nets{$nname};
-			return &Janus::jmsg($nick, "Network not found") unless $dnet && $chan->is_on($dnet);
-			$cname = $chan->str($dnet);
-			$claim{$nname}{$cname} = 1;
-			delete $claim{$nname}{$cname} if $off;
-			delete $cache[$$chan];
-			&Janus::jmsg($nick, "Done");
+		my $nhome = $nick->homenet;
+		my($cname, $claims) = $_[0] =~ /(#\S*)(?: (\S+))?/;
+		my $chan = $nhome->chan($cname) or return;
+		my $chnet = $chan->homenet;
+		my $chnn = $chnet->name;
+		my $chname = $chan->str($chnet);
+		if ($claims) {
+			if ($chnet != $nhome) {
+				&Janus::jmsg($nick, 'Manipulating claims must be done by the owning network');
+				return;
+			}
+			if ($claims =~ s/^-//) {
+				delete $claim{$chnn}{$chname};
+				&Janus::jmsg($nick, 'Deleted');
+			} else {
+				my %n;
+				$n{$_}++ for split /,/, $claims;
+				$n{$chnn}++;
+				$claim{$chnn}{$chname} = join ',', sort keys %n;
+				&Janus::jmsg($nick, 'Set to '.$claim{$chnn}{$chname});
+			}
 		} else {
-			my $nets = resolve($chan);
-			&Janus::jmsg($nick, "Channel $cname is claimed by: $nets");
+			my $nets = $claim{$chnn}{$chname};
+			if ($nets) {
+				&Janus::jmsg($nick, "Channel $cname is claimed by: $nets");
+			} else {
+				&Janus::jmsg($nick, "Channel $cname is not claimed");
+			}
 		}
 	},
 });
@@ -63,9 +57,10 @@ sub acl_ok {
 	my $act = shift;
 	my $src = $act->{src} or return 1;
 	my $chan = $act->{dst};
-	my $home = resolve($chan) or return 1;
-	my $snet = $src->isa('Network') ? $src : $src->homenet();
-	$snet->name() eq $_ and return 1 for split /,/, $home;
+	my $hnet = $chan->homenet;
+	my $claim = $claim{$hnet->name}{$chan->str($hnet)} or return 1;
+	my $snet = $src->isa('Network') ? $src : $src->homenet;
+	$snet->name() eq $_ and return 1 for split /,/, $claim;
 	if ($src->isa('Nick')) {
 		# this is not a true operoverride check, just makes sure acting users
 		# have >= halfop. This is really good enough, if you have halfop you
@@ -104,41 +99,21 @@ sub acl_ok {
 		}
 		$net->send(\%nact);
 		1;
-	}, KICK => check => sub {
+	}, KICK => act => sub {
 		my $act = shift;
 		return undef if acl_ok($act);
 		return undef if $act->{nojlink}; # this is a slight hack, prevents reverting kills
 		my $net = $act->{except};
 		my $chan = $act->{dst};
+		my $src = $act->{src};
 
-		my $kicked = $act->{kickee};
-		my $khome = $kicked->homenet();
-		return undef if $khome eq $net; # I can't stop you kicking your own users
-
-		if ($net->isa('RemoteJanus')) {
-			# TODO bouncing a kick of a remote user by a remote user is a bad idea, but
-			# should probably still be prevented. This requires sync of claims.
-			return undef if $net->jparent($khome->jlink());
-
-			# we have to resync network memberships.
-			# If we send connects that we don't need to, it won't matter as
-			# they will just be dropped.
-			for my $rnet ($chan->nets()) {
-				next unless $net->jparent($rnet->jlink());
-				$net->send(+{
-					type => 'CONNECT',
-					dst => $kicked,
-					net => $rnet,
-				});
-			}
-		}
-		$net->send(+{
-			type => 'JOIN',
-			src => $kicked,
+		&Janus::append({
+			type => 'KICK',
+			src => $Interface::janus,
 			dst => $chan,
-			mode => $chan->get_nmode($kicked),
+			kickee => $src,
+			msg => 'This channel is claimed. You should not kick people in it.',
 		});
-		1;
 	}, TOPIC => check => sub {
 		my $act = shift;
 		return undef if acl_ok($act);
