22506bb61efb4c960a3504a72bd9aed375d5b5d8
Daniel De Graaf <daniel@epsilon>
2008-07-21 22:59:22 -0500

    Fix Commands::* in taint mode

--- src/Commands/*.pm
+++ src/Commands/*.pm
@@ -5,7 +5,8 @@ opendir my $cmd, 'src/Commands' or die;
 while ($_ = readdir $cmd) {
 	s/\.pm$// or next;
 	next if $_ eq '*' || !$_;
-	&Janus::reload("Commands::$_");
+	/^([0-9a-zA-Z_]+)$/ or warn "Bad name $_";
+	&Janus::reload("Commands::$1");
 }
 closedir $cmd;
 
