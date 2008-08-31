19e4b3afc719457cffe20a13d493dc4ee4f5e24f
Daniel De Graaf <daniel@epsilon>
2008-08-31 11:29:15 -0500

    Add KeepMode module
    Persists modes across a janus restart - most useful for setting modes
    not supported by the home ircd.

---
+++ src/Modules/KeepMode.pm
@@ -0,0 +1,40 @@
+# Copyright (C) 2008 Daniel De Graaf
+# Released under the GNU Affero General Public License v3
+package Modules::KeepMode;
+use strict;
+use warnings;
+use Persist;
+
+our %mode;
+&Janus::save_vars(mode => \%mode);
+
+&Event::hook_add(
+	CHANLINK => act => sub {
+		my $act = shift;
+		return unless $act->{net} == $Interface::network;
+		my $chan = $act->{dst};
+		my $hnet = $chan->homenet;
+		return if $hnet->jlink;
+		my $cname = $chan->str($hnet);
+		my $mcache = $mode{$hnet->name}{$cname} or return;
+		&Event::append({
+			type => 'MODE',
+			src => $Interface::janus,
+			dst => $chan,
+			mode => $mcache->[0],
+			args => $mcache->[1],
+			dirs => $mcache->[2],
+		});
+	},
+	MODE => cleanup => sub {
+		my $act = shift;
+		my $chan = $act->{dst};
+		return unless $chan->is_on($Interface::network);
+		my $hnet = $chan->homenet;
+		return if $hnet->jlink;
+		my $cname = $chan->str($hnet);
+		$mode{$hnet->name}{$cname} = [ &Modes::dump($chan) ];
+	}
+);
+
+1;
