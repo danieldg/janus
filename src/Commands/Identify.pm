287848003abbd6b962567846391f35206578145a
Daniel De Graaf <daniel@epsilon>
2008-10-07 16:31:12 -0500

    Hash stored account passwords

--- src/Commands/Identify.pm
+++ src/Commands/Identify.pm
@@ -5,6 +5,28 @@ use strict;
 use warnings;
 use Account;
 
+sub gen_salt {
+	my($nick,$acct) = @_;
+	my $h = $Janus::new_sha1->();
+	# perl's rand is initialized with 32 bits of entropy from urandom;
+	# the other items are to reduce correlation between people setting
+	# their passwords during the same run of the server
+	$h->add(rand . $Janus::time . $nick->gid . '!' . $acct);
+	substr $h->b64digest, 0, 8;
+}
+
+# avoid a dependency on Digest::HMAC_SHA1
+sub hash {
+	my($pass, $salt) = @_;
+	my $h = $Janus::new_sha1->();
+	$salt =~ s/(.)/chr(0x36 ^ ord $1)/eg;
+	$h->add($salt)->add($pass);
+	my $v1 = $h->digest;
+	$salt =~ s/(.)/chr(0x6A ^ ord $1)/eg; # HMAC spec says 5c = 6a^36
+	$h->add($salt)->add($v1);
+	$h->b64digest;
+}
+
 &Janus::command_add({
 	cmd => 'identify',
 	help => 'Identify yourself to janus',
@@ -20,8 +42,9 @@ use Account;
 		my $pass = $_[-1];
 		$user =~ s/[^0-9a-z_]//g;
 		if ($Account::accounts{$user}) {
-			# TODO hash the password
-			if ($Account::accounts{$user}{pass} eq $pass) {
+			my $salt = $Account::accounts{$user}{salt} || '';
+			my $hash = hash($pass, $salt);
+			if ($Account::accounts{$user}{pass} eq $hash) {
 				my $id = $RemoteJanus::self->id;
 				&Log::info($nick->netnick. ' identified as '.$user);
 				&Janus::append({
@@ -60,7 +83,10 @@ use Account;
 		} else {
 			return &Janus::jmsg($dst, 'You can only change your own password');
 		}
-		$acct->{pass} = $_[-1];
+		my $salt = gen_salt($src, $user);
+		my $hash = hash($_[-1], $salt);
+		$acct->{salt} = $salt;
+		$acct->{pass} = $hash;
 		&Janus::jmsg($dst, 'Done');
 	},
 });
