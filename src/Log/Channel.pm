2501098f8b12b0d1ad6c6d249ad226837152ba3f
Daniel De Graaf <daniel@epsilon>
2008-11-15 14:39:27 -0600

    Prevent Log::Channel from recursing when a bad (or no) filter is used

--- src/Log/Channel.pm
+++ src/Log/Channel.pm
@@ -9,11 +9,15 @@ use Scalar::Util 'weaken';
 use POSIX 'strftime';
 use Persist 'Log::Base';
 
+our $loop = 0;
+
 sub output {
 	my $log = shift;
 	$log->name =~ /^(.*?)(#.*)$/ or return;
 	my $net = $Janus::nets{$1} or return;
 	my $chan = $net->chan($2) or return;
+	return if $loop == $Janus::time;
+	$loop = $Janus::time;
 	my $msg = sprintf "\003\%02d\x1f\%s\x1f \%s", @_;
 	&Event::insert_full(map +{
 		type => 'MSG',
@@ -22,6 +26,7 @@ sub output {
 		msgtype => 'PRIVMSG',
 		msg => $_
 	}, split /[\r\n]+/, $msg);
+	$loop = 0;
 }
 
 1;
