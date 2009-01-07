5a416bfb3041f4d29bf68a93d48136c4a49bd8d6
Daniel De Graaf <daniel@epsilon>
2009-01-06 19:03:56 -0600

    Better error checking for SHOWSOURCE

--- src/Commands/ShowSource.pm
+++ src/Commands/ShowSource.pm
@@ -11,14 +11,15 @@ use warnings;
 	details => [
 		"\002SHOWSOURCE\002 module line[-line]",
 	],
+	api => '=src =replyto $ $',
 	code => sub {
 		my($src, $dst, $mod, $line) = @_;
-		$mod =~ /([0-9A-Za-z:_*]+)/ or return &Janus::jmsg($dst, "Use: showsource module line");
+		$mod =~ /([0-9A-Za-z:_*]+)/ or return &Janus::jmsg($dst, "Use: showsource module line[-line]");
 		my $fn = "src/$1.pm";
 		$fn =~ s#::#/#g;
 		open my $f, $fn or return &Janus::jmsg($dst, "Cannot open module file");
 		my($s,$e) = ($line =~ /(\d+)(?:-(\d+))?/);
-		return &Janus::jmsg($dst, "Use: showsource module line") unless $s;
+		return &Janus::jmsg($dst, "Use: showsource module line[-line]") unless $s;
 		$e ||= $s;
 		for (1..($s-1)) {
 			return unless defined <$f>;
