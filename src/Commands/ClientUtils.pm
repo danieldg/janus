f873b3002042a36d741af6686611d3e001e1fd64
Daniel De Graaf <daniel@epsilon>
2009-02-10 16:31:05 -0600

    Add BOTNICK command, make Commands::ClientUtils

---
+++ src/Commands/ClientUtils.pm
@@ -0,0 +1,48 @@
+# Copyright (C) 2007-2008 Nima Gardideh
+# Copyright (C) 2007-2009 Daniel De Graaf
+# Released under the GNU Affero General Public License v3
+package Commands::ClientUtils;
+use strict;
+use warnings;
+
+&Event::command_add({
+	cmd => 'botnick',
+	help => 'Changes the nick of a ClientBot',
+	section => 'Network',
+	details => [
+		'Syntax: BOTNICK network newnick',
+	],
+	acl => 'botnick',
+	api => '=replyto localnet $',
+	code => sub {
+		my($dst,$net,$nick) = @_;
+		return &Janus::jmsg($dst, "Network must be a ClientBot.") unless $net->isa('Server::ClientBot');
+		$net->send("NICK $nick");
+		&Janus::jmsg($dst, 'Done');
+	}
+}, {
+	cmd => 'forceid',
+	help => 'Forcibly tries to identify a ClientBot to services',
+	section => 'Network',
+	details => [
+		"Syntax: \002FORCEID\002 network",
+	],
+	acl => 'forceid',
+	api => '=replyto localnet',
+	code => sub {
+		my($dst, $net) = @_;
+		return &Janus::jmsg($dst, "Network must be a ClientBot.") unless $net->isa('Server::ClientBot');
+		if ($net->param('nspass') || $net->param('qauth')) {
+			&Janus::jmsg($dst, 'Done');
+		} else {
+			&Janus::jmsg($dst, "Network has no identify method configured");
+			return;
+		}
+		&Event::append(+{
+			type => 'IDENTIFY',
+			dst => $net,
+		});
+	},
+});
+
+1;
