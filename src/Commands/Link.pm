4ce874a3af2f4bfc9ecf975b71f4f58a0bbe0bd2
Daniel De Graaf <daniel@epsilon>
2009-02-10 16:53:10 -0600

    Rename ACL command to LINKACL, clarify help

--- src/Commands/Link.pm
+++ src/Commands/Link.pm
@@ -136,6 +136,115 @@ use warnings;
 		});
 		&Janus::jmsg($dst, 'Done');
 	},
+}, {
+	cmd => 'linkacl',
+	help => 'Manages access control for shared channels',
+	section => 'Channel',
+	details => [
+		"\002LINKACL LIST\002 #channel               Lists ACL entries for the channel",
+		"\002LINKACL ALLOW\002 #channel net          Allows a network access to link",
+		"\002LINKACL DENY\002 #channel net           Denies a network access to link",
+		"\002LINKACL DEL\002 #channel net            Removes a network's ACL entry",
+		"\002LINKACL DEFAULT\002 #channel allow/deny Sets the default access for networks",
+	],
+	api => '=src =replyto $ localchan ?$',
+	code => sub {
+		my($src,$dst, $m, $chan, $arg) = @_;
+		$m = lc $m;
+		my $acl = $m eq 'list' ? 'info' : 'create';
+		return unless &Account::chan_access_chk($src, $chan, $acl, $dst);
+		my $hn = $chan->homenet;
+		my $cname = lc $chan->str($hn);
+		my $ifo = $Link::request{$hn->name}{$cname};
+		unless ($ifo && $ifo->{mode}) {
+			&Interface::jmsg($dst, 'That channel is not shared');
+			return;
+		}
+		if ($m eq 'list') {
+			&Interface::jmsg($dst, 'Default: '.($ifo->{mode} == 1 ? 'allow' : 'deny'));
+			return unless $ifo->{ack};
+			for my $nn (keys %{$ifo->{ack}}) {
+				&Interface::jmsg($dst, sprintf '%8s %s', $nn,
+					($ifo->{ack}{$nn} == 1 ? 'allow' : 'deny'));
+			}
+		} elsif ($m eq 'allow') {
+			return &Interface::jmsg($dst, 'Cannot find that network') unless $Janus::nets{$arg};
+			$ifo->{ack}{$arg} = 1;
+			&Interface::jmsg($dst, 'Done');
+		} elsif ($m eq 'deny') {
+			return &Interface::jmsg($dst, 'Cannot find that network') unless $Janus::nets{$arg};
+			$ifo->{ack}{$arg} = 2;
+			&Interface::jmsg($dst, 'Done');
+		} elsif ($m eq 'del' && $arg) {
+			if (delete $ifo->{ack}{$arg}) {
+				&Interface::jmsg($dst, 'Deleted');
+			} else {
+				&Interface::jmsg($dst, 'Cannot find that network');
+			}
+		} elsif ($m eq 'default' && $arg) {
+			if ($arg eq 'allow') {
+				$ifo->{mode} = 1;
+			} elsif ($arg eq 'deny') {
+				$ifo->{mode} = 2;
+			} else {
+				return Interface::jmsg($dst, 'Invalid default ACL');
+			}
+			Interface::jmsg($dst, 'Done');
+		} else {
+			Interface::jmsg($dst, 'Invalid command, see help linkacl');
+		}
+	}
+}, {
+	cmd => 'accept',
+	help => 'Links a channel to a network that has previously requested a link',
+	section => 'Channel',
+	details => [
+		"Syntax: \002ACCEPT\002 #channel net",
+		'Links a channel to a network that has previously requested a link.',
+		'This command is useful after changing a channel ACL, or if the link',
+		'request was made before the destination channel was created.',
+	],
+	api => 'act =src =replyto =tochan homenet chan net',
+	code => sub {
+		my($genact, $src, $dst, $tochan, $snet, $chan, $dnet) = @_;
+		my $dnname = $dnet->name;
+		unless ($tochan) {
+			&Janus::jmsg('Run this command on your own server') if $snet->jlink;
+
+			return unless &Account::chan_access_chk($src, $chan, 'create', $dst);
+			$tochan = lc $chan->homename;
+
+			my $difo = $Link::request{$snet->name}{$tochan};
+			unless ($difo && $difo->{mode}) {
+				&Janus::jmsg($dst, 'That channel is not shared');
+				return;
+			}
+			if ($difo->{mode} == 2) {
+				$difo->{ack}{$dnname} = 1;
+			} else {
+				delete $difo->{ack}{$dnname};
+			}
+
+			&Event::reroute_cmd($genact, $dnet->jlink);
+		}
+		return if $dnet->jlink;
+		my $hname = $snet->name;
+		for my $dcname (keys %{$Link::request{$dnname}}) {
+			my $ifo = $Link::request{$dnname}{$dcname};
+			next if $ifo->{mode};
+			next unless $ifo->{net} eq $hname && lc $ifo->{chan} eq $tochan;
+			my $chan = $dnet->chan($dcname, 1);
+			&Event::append(+{
+				type => 'LINKREQ',
+				chan => $chan,
+				dst => $snet,
+				dlink => $tochan,
+				reqby => $ifo->{mask},
+				reqtime => $ifo->{time},
+				linkfile => 1,
+			});
+		}
+	},
 });
 
 1;
