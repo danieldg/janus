c84c447499ccbe5ab4ca969a2ba6ab0f775c7f3d
Daniel De Graaf <daniel@epsilon>
2009-05-21 12:36:03 -0500

    Move LINKED command from Commands::Network to Commands::List

--- src/Commands/List.pm
+++ src/Commands/List.pm
@@ -30,6 +30,46 @@ Event::command_add({
 		unshift @lines, [ 'Channel', 'Net', ($detail ? ('Created by', 'Created on') : ()) ];
 		Interface::msgtable($dst, \@lines);
 	},
+}, {
+	cmd => 'linked',
+	help => 'Shows a list of the linked networks and channels',
+	section => 'Info',
+	api => '=src =replyto localdefnet',
+	syntax => '[<network>]',
+	code => sub {
+		my($src, $dst, $hnet) = @_;
+		my $hnetn = $hnet->name();
+		my %chans;
+		for my $chan ($hnet->all_chans()) {
+			my %nets = map { $$_ => $_ } $chan->nets();
+			delete $nets{$$hnet};
+			delete $nets{$$Interface::network};
+			next unless scalar keys %nets;
+			my $cnet = $chan->homenet();
+			my $cname = $chan->lstr($cnet);
+			my $hname = $chan->lstr($hnet);
+			my $hcol;
+			my @list = ($hnetn);
+			if ($hnet == $cnet) {
+				$hcol = "\002$hnetn\002";
+				@list = ();
+			} elsif ($cname eq $hname) {
+				$hcol = "\002".$cnet->name()."\002";
+			} else {
+				$hcol = "\002".$cnet->name()."$cname\002";
+			}
+			for my $net (values %nets) {
+				next if $net == $hnet || $net == $cnet;
+				my $oname = $chan->lstr($net);
+				push @list, $net->name().($cname eq $oname ? '' : $oname);
+			}
+			$chans{$hname} = [ $hname, $hcol, join ' ', sort @list ];
+		}
+		my @table = map $chans{$_}, sort keys %chans;
+		my $netlist = join ' ', grep !($_ eq 'janus' || $_ eq $hnetn), sort keys %Janus::nets;
+		unshift @table, [ 'Linked Networks:', "\002$hnetn\002", $netlist ];
+		Interface::msgtable($dst, \@table);
+	}
 });
 
 1;
